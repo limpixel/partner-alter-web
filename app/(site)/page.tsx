f4560b24412631619284fd1bd378193f044c26d6
limpixel <neonjake019@gmail.com>
2025-08-27 21:48:28 +0700

    update SEO Title & navicon

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,20 +1,13 @@
 import { Metadata } from "next";
 import Hero from "@/components/Hero";
-import Brands from "@/components/Brands";
 import Feature from "@/components/Features";
-import About from "@/components/About";
-import FeaturesTab from "@/components/FeaturesTab";
-import FunFact from "@/components/FunFact";
-import Integration from "@/components/Integration";
-import CTA from "@/components/CTA";
-import FAQ from "@/components/FAQ";
-import Pricing from "@/components/Pricing";
-import Contact from "@/components/Contact";
-import Blog from "@/components/Blog";
-import Testimonial from "@/components/Testimonial";
+
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
+  title: "Partner Alter",
+  icons: {
+    icon: "/images/logo/logo-dark-v2.svg", // letakkan favicon di folder public/favicon.ico
+  },
 
   // other metadata
   description: "This is Home for Solid Pro"
